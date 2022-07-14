c5ca5ceddb494175666160822ba3d2b27ebb6579
fazeli-ch <fazeelamushtaq076@gmail.com>
2022-07-14 10:05:39 -0400

    first commit

--- lib/aws-s3-stack.ts
+++ lib/aws-s3-stack.ts
@@ -1,16 +1,49 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "@aws-cdk/core"
+import * as s3 from "@aws-cdk/aws-s3";
+import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
 
-export class AwsS3Stack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+
+export class AwsS3Stack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsS3Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // create a bucket to upload your app files
+
+    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
+      versioned: true,
+    });
+
+    // create a CDN to deploy your website
+
+    const distribution = new cloudfront.Distribution(this, "Distribution", {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket),
+      },
+      defaultRootObject: "index.html",
+    });
+
+    // Print out the web endpoint to the terminal 
+
+    new cdk.CfnOutput(this, "DistributionDomainName", {
+      value: distribution.domainName,
+    });
+
+    // housekeeping for uploading the data in bucket4
+
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("./website")],
+      destinationBucket: websiteBucket,
+      distribution,
+      distributionPaths: ["/*"],
+
+    });
+
+
+   
   }
+  
 }
