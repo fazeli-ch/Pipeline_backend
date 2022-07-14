b19d54fe716d3d848f0b6ebcff52927d1b1bd2ee
fazeli-ch <fazeelamushtaq076@gmail.com>
2022-07-14 13:13:30 -0400

    updated

--- lib/aws-s3-stack.ts
+++ lib/aws-s3-stack.ts
@@ -1,49 +1,17 @@
-import * as cdk from "@aws-cdk/core"
-import * as s3 from "@aws-cdk/aws-s3";
-import * as cloudfront from "@aws-cdk/aws-cloudfront";
-import * as origins from "@aws-cdk/aws-cloudfront-origins";
-import * as s3deploy from "@aws-cdk/aws-s3-deployment";
-
+import * as cdk from "@aws-cdk/core";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
 
 export class AwsS3Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // create a bucket to upload your app files
-
-    const websiteBucket = new s3.Bucket(this, "WebsiteBucket", {
-      versioned: true,
-    });
-
-    // create a CDN to deploy your website
-
-    const distribution = new cloudfront.Distribution(this, "Distribution", {
-      defaultBehavior: {
-        origin: new origins.S3Origin(websiteBucket),
+    const table1 = new dynamodb.Table(this,"Fazeela_table",{
+      tableName:"Fazeela_tb",
+      partitionKey:{
+        name: "id",
+        type: dynamodb.AttributeType.STRING
       },
-      defaultRootObject: "index.html",
     });
-
-    // Print out the web endpoint to the terminal 
-
-    new cdk.CfnOutput(this, "DistributionDomainName", {
-      value: distribution.domainName,
-    });
-
-    // housekeeping for uploading the data in bucket4
-
-    new s3deploy.BucketDeployment(this, "DeployWebsite", {
-      sources: [s3deploy.Source.asset("./website")],
-      destinationBucket: websiteBucket,
-      distribution,
-      distributionPaths: ["/*"],
-
-    });
-
-
-   
+    
   }
-  
 }
